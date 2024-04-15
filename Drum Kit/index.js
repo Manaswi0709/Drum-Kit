867c3b50bac40b9b89a415f29177eb45dbdf5e36
Manaswi0709 <122388953+Manaswi0709@users.noreply.github.com>
2024-04-15 16:22:48 +0530

    Add files via upload

---
+++ Drum Kit/index.js
@@ -0,0 +1,87 @@
+// document.querySelectorAll("button").addEventListener("click",handleClick);
+
+// function handleClick(){
+//     alert("I got clicked!");
+// }
+
+
+//OR
+
+// document.querySelector("button").addEventListener("click",function(){
+//     alert("i got clicked");
+// })
+
+//--For all buttons---
+// var length = document.querySelectorAll("button").length;
+// for(var i=0;i<length;i++)
+// {
+//     document.querySelectorAll("button")[i].addEventListener("click",function(){
+//         alert("i got clicked");
+//     })
+// }
+
+
+//SOUNDS
+
+var length = document.querySelectorAll("button").length;
+for(var i=0;i<length;i++)
+{
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        var buttonInnneHTML = this.innerHTML;
+        
+        makeSound(buttonInnneHTML);
+        buttonAnimation(buttonInnneHTML);
+    })
+}
+
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key)
+{
+    switch (key) {
+        case "w":
+            var tom1 =  new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 =  new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 =  new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 =  new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare =  new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash =  new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick =  new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(currentKey){
+    var activation = document.querySelector("."+currentKey);
+    activation.classList.add("pressed");
+
+    setTimeout(function(){
+        activation.classList.remove("pressed");
+    },100)
+}
+
